a2d2c47925ddafdeb67b4ecabf1446ea1a60205c
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-07-03 04:32:09 -0400

    NewsFeed App

---
+++ app/src/main/java/com/example/android/newsapp/News.java
@@ -0,0 +1,47 @@
+package com.example.android.newsapp;
+
+public class News {
+
+    private String title;
+    private final String NO_IMAGE = "No image";
+    private String imageAddress = NO_IMAGE;
+    private boolean hasImage;
+    private String webAddress;
+
+    News(String title, String imageAddress, String webAddress) {
+        this.title = title;
+        this.imageAddress = imageAddress;
+        this.webAddress = webAddress;
+    }
+
+    News() {
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setImageAddress(String imageAddress) {
+        this.imageAddress = imageAddress;
+    }
+
+    public String getImageAddress() {
+        return imageAddress;
+    }
+
+    public boolean hasImage() {
+        return (!imageAddress.equals(NO_IMAGE));
+    }
+
+    public void setWebAddress(String webAddress) {
+        this.webAddress = webAddress;
+    }
+
+    public String getWebAddress() {
+        return webAddress;
+    }
+}
